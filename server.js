f17f1b6dc7c90f844783216a6bf00c494d31c213
satvik-vm <satvik.verma@iiitb.ac.in>
2023-07-17 20:27:36 +0530

    changed project type to module

--- server.js
+++ server.js
@@ -1,19 +1,32 @@
-require('dotenv').config();
+// require('dotenv').config();
+import dotenv from 'dotenv'
+dotenv.config()
 
-const express = require('express');
+// const express = require('express');
+import express from 'express'
 import { Client } from '@duosecurity/duo_universal';
 
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-	console.log(`server is listening on ${PORT}`)
+	console.log(`Server is listening on ${PORT}`)
 });
 
-const client = new Client({
-	clientId: process.env.,
-	clientSecret: 'yourDuoApplicationSecret',
-	apiHost: 'api-12345678.duosecurity.com',
-	redirectUrl: 'http://localhost:3000/redirect',
-});
+app.get("/", (req, res) => {
+	res.status(200).json({
+		name: "satvik",
+		type: "string"
+	})
+})
+
+// const client = new Client({
+// 	clientId: process.env.INTEGRATION_KEY,
+// 	clientSecret: process.env.SECRET_KEY,
+// 	apiHost: process.env.API_HOSTNAME,
+// 	redirectUrl: 'http://localhost:3000/redirect',
+// });
+
+// const status = await client.healthCheck();
+// console.log(status)
